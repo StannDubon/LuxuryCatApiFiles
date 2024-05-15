1f05811eb59a7faa7c04b86da1edee60b0e7dd81
Fernando <jfer.orantes@gmail.com>
2024-05-15 10:05:21 -0600

    comentarios

--- models/handler/comentario_handler.php
+++ models/handler/comentario_handler.php
@@ -1,6 +1,6 @@
 <?php
 require_once('../../helpers/database.php');
-class CategoriaHandler
+class ComentarioHandler
 {
     protected $id = null;
     protected $nombre = null;
@@ -12,7 +12,7 @@ class CategoriaHandler
     public function searchRows()
     {
         $value = '%' . Validator::getSearchValue() . '%';
-        $sql = 'SELECT c.comentario_id AS id_comentario,
+        $sql = "SELECT c.comentario_id AS id_comentario,
                 CONCAT(u.usuario_nombre, ' ', u.usuario_apellido) AS cliente, p.producto_nombre AS producto, c.comentario_fecha AS fecha_del_comentario, c.comentario_texto AS comentario
                 FROM tb_comentarios c
                 INNER JOIN tb_detalles_pedidos dp ON c.detalle_pedido_id = dp.detalle_pedido_id
@@ -23,9 +23,9 @@ class CategoriaHandler
                 CONCAT(u.usuario_nombre, ' ', u.usuario_apellido) LIKE ? AND
                 p.producto_nombre LIKE ? AND
                 c.comentario_fecha LIKE ? AND
-                c.comentario_texto LIKE ?";
-                ORDER BY id_comentario';
-        $params = array($value, $value);
+                c.comentario_texto LIKE ?
+                ORDER BY id_comentario";
+        $params = array($value, $value, $value, $value, $value);
         return Database::getRows($sql, $params);
     }
 
@@ -43,11 +43,24 @@ class CategoriaHandler
 
     public function readOne()
     {
-        $sql = 'SELECT categoria_id, categoria_nombre, categoria_descripcion, categoria_estado
-                FROM tb_categorias
-                WHERE categoria_id = ?';
+        $sql = 'SELECT c.comentario_id AS id_comentario,
+                CONCAT(u.usuario_nombre, ' ', u.usuario_apellido) AS cliente, p.producto_nombre AS producto, c.comentario_fecha AS fecha_del_comentario, c.comentario_texto AS comentario
+                FROM tb_comentarios c
+                INNER JOIN tb_detalles_pedidos dp ON c.detalle_pedido_id = dp.detalle_pedido_id
+                INNER JOIN tb_productos p ON dp.producto_id = p.producto_id
+                INNER JOIN tb_pedidos ped ON dp.pedido_id = ped.pedido_id
+                INNER JOIN tb_usuarios u ON ped.usuario_id = u.usuario_id; where comentario_id = ?';
         $params = array($this->id);
         return Database::getRow($sql, $params);
     }
+
+    public function changeStatus()
+    {
+        $sql = 'UPDATE tb_comentarios SET comentario_estado = NOT comentario_estado WHERE comentario_id=?;';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+
 }
 ?>
